a8b5e2adedac70baa97d80321c8ccd16560c29ce
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2024-04-13 15:54:53 +0700

    Fetching Cabin Data

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,7 @@
+/* eslint-disable react/prop-types */
+
 import styled from "styled-components";
+import { formatCurrency } from "../../utils/helpers";
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +41,20 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+function CabinRow({ cabin }) {
+  const { name, maxCapacity, regularPrice, discount, img } = cabin;
+
+  return (
+    <TableRow role="row">
+      <Img src={img} />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity}</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}</Discount>
+      <button>Delete</button>
+    </TableRow>
+  );
+}
+
+export default CabinRow;
